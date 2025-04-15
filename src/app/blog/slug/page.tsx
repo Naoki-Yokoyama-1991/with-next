566792dfc25e8674aea6e4d9012d0662629a87b6
yoko <kionako96@gmail.com>
2025-04-15 11:53:04 +0900

    Update project structure and remove unused assets

--- src/app/blog/slug/page.tsx
+++ src/app/blog/slug/page.tsx
@@ -1,9 +1,5 @@
 // app/blog/[slug]/page.tsx
 
-// export default function BlogDetailPage({
-//   params,
-// }: {
-//   params: { slug: string };
-// }) {
-//   return <main>Blog Detail Page: {params.slug}</main>;
-// }
+export default function BlogDetailPage() {
+  return <main>slug Page</main>;
+}
